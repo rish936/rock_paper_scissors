f5068b52164d2d57c08618343ce31b4f92fd6be1
Rishabh Tiwari <rish936@gmail.com>
2023-04-22 11:49:00 +0530

    Added the logic to play the game 5 times

--- game.js
+++ game.js
@@ -25,51 +25,100 @@ function getPlayerSelection() {
     
 }
 
-let playerChoice = getPlayerSelection();
-let computerChoice = getComputerChoice();
+// let playerChoice = getPlayerSelection();
+// let computerChoice = getComputerChoice();
 
 function gameRound(playerChoice, computerChoice)  {
     if (playerChoice == 'rock')  {
         if (computerChoice == 'scissors') {
-            return("You win. Rock beats Scissors");
+            // return("You win. Rock beats Scissors");
+            return true;
         }
 
         else if (computerChoice == 'rock') {
-            return("Draw! You both select rock");
+            // return("Draw! You both select rock");
+            return ("Draw");
         }
 
         else    {
-            return("You lose. LMAO NOOB. Paper beats Rock");
+            // return("You lose. LMAO NOOB. Paper beats Rock");
+            return false;
         }
     }
 
     else if (playerChoice == 'scissors')  {
         if (computerChoice == 'scissors') {
-            return("Draw! You both select Scissors");
+            // return("Draw! You both select Scissors");
+            return ("Draw");
         }
 
         else if (computerChoice == 'rock') {
-            return("You lose. LMAO NOOB. Rock beats Scissors");
+            // return("You lose. LMAO NOOB. Rock beats Scissors");
+            return false;
         }
 
         else    {
-            return("You win. Scissors beats Paper");
+            // return("You win. Scissors beats Paper");
+            return true;
         }
     }
 
     else if (playerChoice == 'paper')  {
         if (computerChoice == 'scissors') {
-            return("You lose. LMAO NOOB. Scissors beats Paper");
+            // return("You lose. LMAO NOOB. Scissors beats Paper");
+            return false;
         }
 
         else if (computerChoice == 'rock') {
-            return("You win. Paper beats Rock");
+            // return("You win. Paper beats Rock");
+            return true;
         }
 
         else    {
-            return("Draw. You both select Paper");
+            // return("Draw. You both select Paper");
+            return ("Draw");
         }
     }
 }
 
-window.alert(gameRound(playerChoice, computerChoice));
+// window.alert(gameRound(playerChoice, computerChoice));
+
+function game() {
+    let playerWin = 0;
+    let computerWin = 0;
+    let draw = 0;
+    for (let index = 0; index < 5; index++) {
+        let playerChoice = getPlayerSelection();
+        let computerChoice = getComputerChoice();
+        let result = gameRound(playerChoice, computerChoice);
+        // window.alert(index);
+        if (result == true) {
+            playerWin++;
+            
+        }
+        else if (result == false) {
+            computerWin++;
+           
+        }
+
+        else    {
+            draw++;
+            
+        }
+        
+    }
+
+    if (playerWin > computerWin) {
+        return("You are pro. You won");
+    }
+
+    else if (computerWin > playerWin) {
+        return("LMAO n00b!!!! You lost.");
+    }
+
+    else    {
+        return("Well played!!! The game is draw");
+    }
+}
+
+window.alert(game());
